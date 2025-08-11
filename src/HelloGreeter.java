20b7f47080feb43341cf6eef8b436021fa0a7fd2
Matt Bennett <144359294+filmteacher@users.noreply.github.com>
2025-08-11 14:52:35 -0400

    Completed files

--- src/HelloGreeter.java
+++ src/HelloGreeter.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class HelloGreeter {
+
+    String greeting;
+
+    public HelloGreeter(String greeting) {
+        this.greeting = greeting;
+    }
+
+    public void showRepeatGreeting(int numgreets)
+        {
+           for (int i = 1; i <= numgreets; i++) {
+               System.out.println(greeting);
+           }
+        }
 }
